d190c9b3153d4f9ba3a67ea856d47890c0499eee
zhanghy <454468165@qq.com>
2020-09-21 19:11:57 +0800

    add:新增面试题

--- 1.js
+++ 1.js
@@ -1,38 +1,25 @@
-var urls = [
-  'http://jsonplaceholder.typicode.com/posts/1',
-  'http://jsonplaceholder.typicode.com/posts/2', 
-  'http://jsonplaceholder.typicode.com/posts/3', 
-  'http://jsonplaceholder.typicode.com/posts/4',
-  'http://jsonplaceholder.typicode.com/posts/5', 
-  'http://jsonplaceholder.typicode.com/posts/6', 
-  'http://jsonplaceholder.typicode.com/posts/7', 
-  'http://jsonplaceholder.typicode.com/posts/8',
-  'http://jsonplaceholder.typicode.com/posts/9', 
-  'http://jsonplaceholder.typicode.com/posts/10'
-]
-
-function loadDate (url) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest()
-    xhr.onload = function () {
-      resolve(xhr.responseText)
-    }
-    xhr.open('GET', url)
-    xhr.send()
-  })
+var length = 10;
+function fn(){
+  console.log(this.length)
 }
 
-function load(){
-  let arr = urls.splice(0,3);
-  let res = false;
-  if(arr.length < 3){
-    res = true;
+var obj = {
+  lenght: 5,
+  method:function(fn){
+    fn();
+    arguments[0]();
   }
-  // console.log(arr,arr.map(v=> loadDate(v)))
-  Promise.all(arr.map(v=> loadDate(v))).then(res=>{
-    if(!res){
-      load()
-    }
-  })
+};
+
+obj.method(fn,1)
+
+function a(xx){
+  this.x = xx;
+  return this;
 }
-load();
+
+var x = a(5);
+var y = a(6);
+ 
+console.log(x.x);
+console.log(y.x);
